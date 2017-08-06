bbad55abc1817e0a18f43acfe6d2ed8d4482f22a
Sean Lee <freshdried@gmail.com>
2017-08-06 14:49:36 -0400

    SignalEvent init

--- src/Syzygy.hs
+++ src/Syzygy.hs
@@ -10,8 +10,11 @@ import Data.Function ((&))
 
 type Time = Rational
 type Interval = (Time, Time) -- left-closed and right-open intervals
-type Event a = (Interval, a)
-type Signal a = Interval -> [Event a] -- A signal is defined by the "integral" of a sampling function
+data SignalEvent a = MkSignalEvent
+  { support :: Interval
+  , event :: a
+  } deriving (Eq, Show)
+type Signal a = Interval -> [SignalEvent a] -- A signal is defined by the "integral" of a sampling function
 
 embed :: a -> Signal a
 embed x (queryStart, queryEnd) = do
@@ -19,19 +22,19 @@ embed x (queryStart, queryEnd) = do
     start = (fromIntegral @Integer) . floor $ queryStart
     end = (fromIntegral @Integer) . ceiling $ queryEnd
   beat <- [start..end - 1]
-  return ((beat, beat + 1), x)
+  return MkSignalEvent { support = (beat, beat + 1), event = x }
 
 
 prune :: Signal a -> Signal a
 prune signal (queryStart, queryEnd) = filter inBounds $ signal (queryStart, queryEnd)
   where
-    inBounds ((s, _), _) = s >= queryStart && s < queryEnd
+    inBounds MkSignalEvent {support = (s, _)} = s >= queryStart && s < queryEnd
 
 -- | shift forward in time
 shift :: Time -> Signal a -> Signal a
 shift t f = f
   & lmap (\(start, end) -> (start - t, end - t))
-  & rmap (\res -> [((start + t, end + t), x) | ((start, end), x) <- res])
+  & rmap (\res -> [MkSignalEvent { support = (start + t, end + t), event = x } | MkSignalEvent { support = (start, end), event = x } <- res])
 
 stack :: [Signal a] -> Signal a
 stack sigs query = do
@@ -48,27 +51,27 @@ interleave sigs query = do
 fast :: Rational -> Signal a -> Signal a
 fast n sig = sig
   & lmap (\(start, end) -> (start * n, end * n))
-  & rmap (\res -> [((start/n, end/n), x) | ((start, end), x) <- res])
+  & rmap (\res -> [MkSignalEvent { support = (start / n, end / n), event = x } | MkSignalEvent { support = (start, end), event = x } <- res])
 
 ap ::  Signal (a -> b) -> Signal a -> Signal b
 ap sigF (prune -> sigX) = prune $ \query0 ->  do
-  (query1, f) <- sigF query0
-  (query2, x) <- sigX query1
-  return (query2, f x)
+  MkSignalEvent { support = query1, event = f } <- sigF query0
+  MkSignalEvent { support = query2, event = x } <- sigX query1
+  return MkSignalEvent { support = query2, event = f x }
 
 -- A Behavior is a continuous function that is defined at every point in the sampling space
-type Behavior a = forall b. (Signal (a -> b) -> Signal b)
-runBehavior :: Behavior (a -> b) -> Signal a -> Signal b
-runBehavior b s = b $ (fmap . fmap . fmap) (flip ($)) s
+-- type Behavior a = forall b. (Signal (a -> b) -> Signal b)
+-- runBehavior :: Behavior (a -> b) -> Signal a -> Signal b
+-- runBehavior b s = b $ (fmap . fmap . fmap) (flip ($)) s
 
-liftContinuous :: (Time -> a) -> Behavior a
-liftContinuous fn sig query = do
-  let events = sig query
-  ((s, e), f) <- events
-  let
-    midpoint = (s + e) * 0.5
-    y = fn midpoint
-  return ((s, e), f y)
+-- liftContinuous :: (Time -> a) -> Behavior a
+-- liftContinuous fn sig query = do
+--   let events = sig query
+--   ((s, e), f) <- events
+--   let
+--     midpoint = (s + e) * 0.5
+--     y = fn midpoint
+--   return ((s, e), f y)
 
-sine :: Behavior Double
-sine = liftContinuous $ sin . fromRational
+-- sine :: Behavior Double
+-- sine = liftContinuous $ sin . fromRational
--- test/SyzygySpec.hs
+++ test/SyzygySpec.hs
@@ -12,26 +12,23 @@ import Data.Monoid
 import Test.QuickCheck
 import Data.Function ((&))
 
-queryWith :: Interval -> Signal a -> [Event a]
-queryWith x p = p x
-
 spec :: Spec
 spec = do
   describe "Syzygy" $ do
     describe "embed" $ do
       let pat = embed ()
       it "should work" $ do
-        pat (0, 1) `shouldBe` [((0, 1), ())]
-        pat (0, 2) `shouldBe` [((0, 1), ()), ((1, 2), ())]
-        pat (0.5, 1.5) `shouldBe` [((0, 1), ()), ((1, 2), ())]
-        pat (0.5, 2.5) `shouldBe` [((0, 1), ()), ((1, 2), ()), ((2, 3), ())]
+        pat (0, 1) `shouldBe` [MkSignalEvent (0, 1) ()]
+        pat (0, 2) `shouldBe` [MkSignalEvent (0, 1) (), MkSignalEvent (1, 2) ()]
+        pat (0.5, 1.5) `shouldBe` [MkSignalEvent (0, 1) (), MkSignalEvent (1, 2) ()]
+        pat (0.5, 2.5) `shouldBe` [MkSignalEvent (0, 1) (), MkSignalEvent (1, 2) (), MkSignalEvent (2, 3) ()]
 
       it "starts of events should be less than query ends" $ property $ \input ->
         let
           ((NonNegative (start :: Rational), NonNegative (dur :: Rational))) = input
           end = start + dur
           events = pat (start, end)
-          offendingEvents = filter (\((s, e), _) -> s >= end) events
+          offendingEvents = filter (\MkSignalEvent { support = (s, _) } -> s >= end) events
         in
           length offendingEvents == 0
 
@@ -40,7 +37,7 @@ spec = do
           ((NonNegative (start :: Rational), NonNegative (dur :: Rational))) = input
           end = start + dur
           events = pat (start, end)
-          offendingEvents = filter (\((s, e), _) -> e <= start) events
+          offendingEvents = filter (\MkSignalEvent { support = (_, e) } -> e <= start) events
         in
           length offendingEvents == 0
 
@@ -59,18 +56,18 @@ spec = do
         (fast 1 pat) (0, 1)  `shouldBe` pat (0, 1)
 
       it "should work for fast 2" $ do
-        (fast 2 pat) (0, 0.5) `shouldBe` [((0, 1/2), ())]
-        (fast 2 pat) (0, 1) `shouldBe` [((0, 1/2), ()), ((1/2, 1), ())]
-        (fast 2 pat) (1, 2) `shouldBe` [((1, 3/2), ()), ((3/2, 2), ())]
+        (fast 2 pat) (0, 0.5) `shouldBe` [MkSignalEvent (0, 1/2) ()]
+        (fast 2 pat) (0, 1) `shouldBe` [MkSignalEvent (0, 1/2) (), MkSignalEvent (1/2, 1) ()]
+        (fast 2 pat) (1, 2) `shouldBe` [MkSignalEvent (1, 3/2) (), MkSignalEvent (3/2, 2) ()]
 
       it "should work for fast 3" $ do
-        (fast 3 pat) (0, 1/3) `shouldBe` [((0, 1/3), ())]
-        (fast 3 pat) (0, 1) `shouldBe` [((0, 1/3), ()), ((1/3, 2/3), ()), ((2/3, 1), ())]
-        (fast 3 pat) (2/3, 4/3) `shouldBe` [((2/3, 1), ()), ((1, 4/3), ())]
+        (fast 3 pat) (0, 1/3) `shouldBe` [MkSignalEvent (0, 1/3) ()]
+        (fast 3 pat) (0, 1) `shouldBe` [MkSignalEvent (0, 1/3) (), MkSignalEvent (1/3, 2/3) (), MkSignalEvent (2/3, 1) ()]
+        (fast 3 pat) (2/3, 4/3) `shouldBe` [MkSignalEvent (2/3, 1) (), MkSignalEvent (1, 4/3) ()]
 
       it "should noop for fast 0.5" $ do
-        (fast 0.5 pat) (0, 1)  `shouldBe` [((0, 2), ())]
-        (fast 0.5 pat) (0, 2)  `shouldBe` [((0, 2), ())]
+        (fast 0.5 pat) (0, 1) `shouldBe` [MkSignalEvent (0, 2) ()]
+        (fast 0.5 pat) (0, 2) `shouldBe` [MkSignalEvent (0, 2) ()]
 
     describe "shift" $ do
       let pat = embed ()
@@ -78,17 +75,22 @@ spec = do
         (shift 0 pat) (0, 1)  `shouldBe` pat (0, 1)
 
       it "should work" $ do
-        (shift 0 pat) (0, 1)  `shouldBe` [((0, 1), ())]
-        (shift 0.5 pat) (0, 1)  `shouldBe` [((-1/2, 1/2), ()), ((1/2, 3/2), ())]
-        (shift 1 pat) (0, 1)  `shouldBe` [((0, 1), ())]
+        (shift 0 pat) (0, 1) `shouldBe` [MkSignalEvent (0, 1) ()]
+        (shift 0.5 pat) (0, 1) `shouldBe` [MkSignalEvent (-1/2, 1/2) (), MkSignalEvent (1/2, 3/2) ()]
+        (shift 1 pat) (0, 1) `shouldBe` [MkSignalEvent (0, 1) ()]
 
       it "should shift forwards in time" $ do
-        (shift 0.25 pat) (0, 1)  `shouldBe` [((-3/4, 1/4), ()), ((1/4, 5/4), ())]
+        (shift 0.25 pat) (0, 1)  `shouldBe` [MkSignalEvent (-3/4, 1/4) (), MkSignalEvent (1/4, 5/4) ()]
 
     describe "stack" $ do
       let pat = embed ()
       it "should stack patterns" $ do
-        stack [(shift 0.25 pat), (shift 0.5 pat)] (0, 1) `shouldBe` [((-3/4, 1/4), ()), ((1/4, 5/4), ()), ((-1/2, 1/2), ()), ((1/2, 3/2), ())]
+        stack [(shift 0.25 pat), (shift 0.5 pat)] (0, 1) `shouldBe`
+          [ MkSignalEvent (-3/4, 1/4) ()
+          , MkSignalEvent (1/4, 5/4) ()
+          , MkSignalEvent (-1/2, 1/2) ()
+          , MkSignalEvent (1/2, 3/2) ()
+          ]
 
     describe "interleave" $ do
       let pat = embed ()
@@ -103,9 +105,4 @@ spec = do
       let pat = embed ()
 
       it "should noop for pure id" $ do
-        (ap (embed id) pat) (0, 1)  `shouldBe` [((0, 1), ())]
-
-      it "should work" $ do
-        let patF = interleave $ embed <$> [("a",), ("b",)]
-        (ap patF pat) (0, 1) `shouldBe` [((0, 1), ("a", ())), ((0, 1), ("b", ()))] -- FIXME: is this really what we want?
-        (ap patF (fast 2 pat)) (0, 1)  `shouldBe` [((0, 1/2), ("a", ())), ((1/2, 1), ("a", ())), ((0, 1/2), ("b", ())), ((1/2, 1), ("b", ()))]
+        (ap (embed id) pat) (0, 1)  `shouldBe` [MkSignalEvent (0, 1) ()]
