fc69ec71a02143c00dc3437250c04e7ee7755db1
sean lee <freshdried@gmail.com>
2017-08-14 00:47:23 -0400

    Simplify Signal type, introduce lawful Monoid instance for Signal (#2)
    * split init
    
    * refactor
    
    * back to native tuple for interval
    
    * fix signal monoid semantics
    
    * clean
    
    * interleave, stack

--- src/Syzygy.hs
+++ src/Syzygy.hs
@@ -9,120 +9,118 @@ module Syzygy where
 import Data.Profunctor
 import Data.Function ((&))
 import Data.Monoid
-import qualified Test.QuickCheck as QC
 
 type Time = Rational
 
--- | left-closed and right-open intervals
-data Interval = MkInterval
-  { start :: Time
-  , end :: Time
-  }
-  deriving (Eq, Show)
+type Interval = (Time, Time)
 
 data Event a = MkEvent
-  { query :: Interval
+  { query :: (Time, Time)
   , payload :: a
   } deriving (Eq, Show, Functor)
 
-data SignalEvent a = MkSignalEvent
-  { support :: Interval
-  , event :: Event a
-  } deriving (Eq, Show, Functor)
-
-type Signal a = Interval -> [SignalEvent a] -- A signal is defined by the "integral" of a sampling function
+newtype Signal a = MkSignal { signal :: Interval -> [Event a] } -- A signal is defined by the "integral" of a sampling function
 
 
-instance Monoid Interval where
-  MkInterval startX endX `mappend` MkInterval startY endY =
-    let lengthX = endX - startX
-    in MkInterval (startX + lengthX * startY) (startX + lengthX * endY)
-  mempty = MkInterval 0 1
+combineEvent :: forall a. Monoid a => Event a -> Event a -> [Event a]
+combineEvent x y = headX <> headY <> overlap <> tailY <> tailX
+  where
+    MkEvent { query = (startX, endX), payload = payloadX } = x
+    MkEvent { query = (startY, endY), payload = payloadY } = y
+    overlap = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadX <> payloadY }
+      where
+        start = max startX startY
+        end = min endX endY
 
-instance QC.Arbitrary Interval where
-  arbitrary = getInterval <$> QC.arbitrary
-    where
-      getInterval :: (QC.NonNegative Rational, QC.NonNegative Rational) -> Interval
-      getInterval (QC.NonNegative start, QC.NonNegative dur) = MkInterval {start, end = start + dur}
+    tailY = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadY }
+      where
+        start = max startY endX
+        end = endY
 
-instance Applicative Event where
-  pure x = MkEvent { query = MkInterval 0 1, payload = x}
+    tailX = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadX }
+      where
+        start = max startX endY
+        end = endX
 
-  MkEvent {query = queryF, payload = f} <*> MkEvent {query = queryX, payload = x} =
-    MkEvent { query = queryF <> queryX, payload = f x }
+    headX = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadX }
+      where
+        start = startX
+        end = min endX startY
 
-instance QC.Arbitrary a => QC.Arbitrary (Event a) where
-  arbitrary = do
-    query <- QC.arbitrary
-    payload <- QC.arbitrary
-    return MkEvent {query, payload}
+    headY = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadY }
+      where
+        start = startY
+        end = min startX endY
 
-split :: SignalEvent (a -> b) -> SignalEvent a -> [SignalEvent b]
-split f x =
-  let
-    MkSignalEvent { support = supportF , event = MkEvent { query = queryF, payload = payloadF } } = f
-    MkSignalEvent { support = supportX, event = MkEvent { query = queryX, payload = payloadX } } = x
-  in
-    do
-      return $ MkSignalEvent { support = supportX, event = MkEvent { query = queryX, payload = payloadF payloadX } }
+combineEventOverlap :: forall a. Monoid a => Event a -> Event a -> [Event a]
+combineEventOverlap x y = overlap
+  where
+    MkEvent { query = (startX, endX), payload = payloadX } = x
+    MkEvent { query = (startY, endY), payload = payloadY } = y
+    overlap = if start >= end then [] else return $ MkEvent { query = (start, end), payload = payloadX <> payloadY }
+      where
+        start = max startX startY
+        end = min endX endY
 
 embed :: a -> Signal a
-embed x (MkInterval queryStart queryEnd) = do
+embed x = MkSignal $ \(queryStart, queryEnd) -> do
   let
     start = (fromIntegral @Integer) . floor $ queryStart
     end = (fromIntegral @Integer) . ceiling $ queryEnd
   beat <- [start..end - 1]
-  return MkSignalEvent { support = MkInterval beat (beat + 1), event = pure x }
+  return MkEvent { query = (beat, (beat + 1)), payload = x }
 
+pruneSignal :: Signal a -> Signal a
+pruneSignal (MkSignal sig) = MkSignal $ \(queryStart, queryEnd) ->
+  let
+    inBounds MkEvent {query = (start, _)} = start >= queryStart && start < queryEnd
+  in
+    filter inBounds $ sig (queryStart, queryEnd)
 
+instance Monoid a => Monoid (Signal a) where
+  mempty = MkSignal $ \_ -> []
 
-prune :: Signal a -> Signal a
-prune signal (MkInterval queryStart queryEnd) = filter inBounds $ signal (MkInterval queryStart queryEnd)
-  where
-    inBounds MkSignalEvent {support = MkInterval{start}} = start >= queryStart && start < queryEnd
+  (MkSignal sigX) `mappend` (MkSignal sigY) = MkSignal $ \query ->
+    let
+      xs = sigX query
+    in
+      case xs of
+        [] -> sigY query
+        _ ->
+          let
+            f x@MkEvent{query=subQuery} acc = case sigY subQuery of
+              [] -> (pure x <> acc)
+              ys -> (<> acc) $ do
+                y <- ys
+                x `combineEventOverlap` y
+          in
+            foldr f [] xs
 
 -- | shift forward in time
 shift :: Time -> Signal a -> Signal a
-shift t f = f
-  & lmap (\MkInterval{start, end} -> MkInterval { start = start - t, end = end - t })
-  & rmap (fmap $ \ev@MkSignalEvent { support = MkInterval start end } -> ev { support = MkInterval (start + t) (end + t) })
-
-stack :: [Signal a] -> Signal a
-stack sigs query = do
-  sig <- sigs
-  sig query
-
-interleave :: [Signal a] -> Signal a
-interleave sigs query = do
-  let (fromIntegral -> len) = length sigs
-  (sig, n) <- zip sigs [0..]
-  shift (n/len) sig query
+shift t MkSignal {signal=originalSignal} = MkSignal {signal}
+  where
+    signal = originalSignal
+      & lmap (\(start, end) -> (start - t, end - t ))
+      & rmap (fmap $ \ev@MkEvent { query = (start, end) } -> ev { query = (start + t, end + t) })
 
 -- | scale faster in time
 fast :: Rational -> Signal a -> Signal a
-fast n sig = sig
-  & lmap (\MkInterval{start, end} -> MkInterval { start = start * n, end = end * n })
-  & rmap (fmap $ \ev@MkSignalEvent { support = MkInterval start end } -> ev { support = MkInterval (start / n) (end / n) })
+fast n MkSignal {signal=originalSignal} = MkSignal {signal}
+  where
+    signal = originalSignal
+      & lmap (\(start, end) -> ( start * n, end * n ))
+      & rmap (fmap $ \ev@MkEvent { query = (start, end) } -> ev { query = (start / n, end / n) })
 
--- ap ::  Signal (a -> b) -> Signal a -> Signal b
--- ap sigF (prune -> sigX) = prune $ \query0 ->  do
---   MkSignalEvent { support = query1, event = f } <- sigF query0
---   MkSignalEvent { support = query2, event = x } <- sigX query1
---   return MkSignalEvent { support = query2, event = f x }
+-- | stack in parallel
+stack :: [Signal a] -> Signal a
+stack sigs = MkSignal $ \query -> do
+  MkSignal{signal} <- sigs
+  signal query
 
--- A Behavior is a continuous function that is defined at every point in the sampling space
--- type Behavior a = forall b. (Signal (a -> b) -> Signal b)
--- runBehavior :: Behavior (a -> b) -> Signal a -> Signal b
--- runBehavior b s = b $ (fmap . fmap . fmap) (flip ($)) s
-
--- liftContinuous :: (Time -> a) -> Behavior a
--- liftContinuous fn sig query = do
---   let events = sig query
---   ((s, e), f) <- events
---   let
---     midpoint = (s + e) * 0.5
---     y = fn midpoint
---   return ((s, e), f y)
-
--- sine :: Behavior Double
--- sine = liftContinuous $ sin . fromRational
+-- | interleave within one period
+interleave :: [Signal a] -> Signal a
+interleave sigs = MkSignal $ \query -> do
+  let (fromIntegral -> len) = length sigs
+  (sig, n) <- zip sigs [0..]
+  signal (shift (n/len) sig) query
--- test/SyzygySpec.hs
+++ test/SyzygySpec.hs
@@ -8,131 +8,191 @@ module SyzygySpec where
 
 import Test.Hspec
 import Syzygy
-import Test.QuickCheck
-import Data.Function ((&))
 import Data.Monoid ((<>))
+import qualified Test.QuickCheck as QC
 
 spec :: Spec
 spec = do
   describe "Syzygy" $ do
-    describe "Interval" $ do
-      describe "Monoid Instance" $ do
-        it "obeys the associative law" $ property $ \(x :: Interval, y :: Interval, z :: Interval) ->
-          (x <> (y <> z)) ==  ((x <> y) <> z)
-
-        it "obeys the left unital law" $ property $ \(x :: Interval) ->
-          (mempty <> x) == x
-
-        it "obeys the right unital law" $ property $ \(x :: Interval) ->
-          (x <> mempty) == x
 
     describe "Event" $ do
-      describe "Applicative Instance" $ do
-        it "obeys the identity law" $ property $ \(event :: Event ()) ->
-          (pure id <*> event) ==  event
+      describe "combineEvent" $ do
+        it "works overlapped 1" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (0, 1), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe` [ MkEvent { query = (0, 1), payload = "HelloWorld" } ]
 
-        it "obeys the homomorphism law" $ do
-          let f = (, "hello")
-          let x = ()
-          (pure f <*> pure x) `shouldBe` pure @Event (f x)
+        it "works overlapped 2" $ do
+          let
+            eventX = MkEvent { query = (0, 0.5), payload = "Hello"}
+            eventY = MkEvent { query = (0, 1), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 0.5), payload = "HelloWorld" }
+            , MkEvent { query = (0.5, 1), payload = "World" }
+            ]
 
-        it "obeys the interchange law" $ property $ \(query :: Interval)->
-          let u = MkEvent { query, payload = (,"hello") }
-          in (u <*> pure ()) == (pure ($()) <*> u)
+        it "works overlapped 3" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (0, 0.5), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 0.5), payload = "HelloWorld" }
+            , MkEvent { query = (0.5, 1), payload = "Hello" }
+            ]
 
-        it "obeys the composition law" $ property $ \(input :: (Event String, Event String, Event String)) ->
-          let (fmap (<>) -> u, fmap (<>) -> v, x) = input
-          in (pure (.) <*> u <*> v <*> x) == (u <*> (v <*> x))
+        it "works overlapped 4" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (0.5, 1), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 0.5), payload = "Hello" }
+            , MkEvent { query = (0.5, 1), payload = "HelloWorld" }
+            ]
 
-    describe "split" $ do
-      it "works" $ do
+        it "works overlapped 5" $ do
+          let
+            eventX = MkEvent { query = (0.5, 1), payload = "Hello"}
+            eventY = MkEvent { query = (0, 1), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 0.5), payload = "World" }
+            , MkEvent { query = (0.5, 1), payload = "HelloWorld" }
+            ]
+
+        it "works overlapped 6" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (0.25, 0.75), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 0.25), payload = "Hello" }
+            , MkEvent { query = (0.25, 0.75), payload = "HelloWorld" }
+            , MkEvent { query = (0.75, 1), payload = "Hello" }
+            ]
+
+        it "works when there are no overlaps" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (1, 2), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 1), payload = "Hello" }
+            , MkEvent { query = (1, 2), payload = "World" }
+            ]
+
+        it "works when there are no overlaps 2" $ do
+          let
+            eventX = MkEvent { query = (0, 1), payload = "Hello"}
+            eventY = MkEvent { query = (2, 3), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 1), payload = "Hello" }
+            , MkEvent { query = (2, 3), payload = "World" }
+            ]
+
+        it "works when there are no overlaps 3" $ do
+          let
+            eventX = MkEvent { query = (2, 3), payload = "Hello"}
+            eventY = MkEvent { query = (0, 1), payload = "World"}
+          (eventX `combineEvent` eventY) `shouldBe`
+            [ MkEvent { query = (0, 1), payload = "World" }
+            , MkEvent { query = (2, 3), payload = "Hello" }
+            ]
+
+    describe "Signal" $ do
+      describe "embed" $ do
+        let pat = embed ()
+        it "should work" $ do
+          signal pat (0, 1) `shouldBe`     [MkEvent (0, 1) ()]
+          signal pat (0, 2) `shouldBe`     [MkEvent (0, 1) (), MkEvent (1, 2) ()]
+          signal pat (0.5, 1.5) `shouldBe` [MkEvent (0, 1) (), MkEvent (1, 2) ()]
+          signal pat (0.5, 2.5) `shouldBe` [MkEvent (0, 1) (), MkEvent (1, 2) (), MkEvent (2, 3) ()]
+
+        it "starts of events should be less than query ends" $ QC.property $ \query@(_, end) ->
+          [] == filter (\MkEvent { query = (s, _) } -> s >= end) (signal pat query)
+
+        it "ends of events should be greater than query starts" $ QC.property $ \query@(start, _) ->
+          [] == filter (\MkEvent { query = (_, e) } -> e <= start) (signal pat query)
+
+        it "should have transparently divisible queries when pruned" $ do
+          let pat = pruneSignal $ embed ()
+          (signal pat (0, 0)   <> signal pat (0, 1)) `shouldBe` signal pat (0, 1)
+          (signal pat (0, 1)   <> signal pat (1, 1)) `shouldBe` signal pat (0, 1)
+          (signal pat (0, 0.5) <> signal pat (0.5, 1.0)) `shouldBe` signal pat (0, 1)
+          (signal pat (0, 0.3) <> signal pat (0.3, 1.3) <> signal pat (1.3, 2)) `shouldBe` signal pat (0, 2)
+
+      describe "Monoid Instance" $ do
         let
-          eventF = MkSignalEvent { support = MkInterval 0 1, event = pure id}
-          eventX = MkSignalEvent { support = MkInterval 0 1, event = pure ()}
-        split eventF eventX `shouldBe` [eventX]
+          shouldEqualSignal :: (Show a, Monoid a, Eq a) => Signal a -> Signal a -> IO ()
+          shouldEqualSignal x y = signal x query `shouldBe` signal y query
+            where
+              query = (0, 1)
 
-      it "works" $ do
-        let
-          eventF = MkSignalEvent { support = MkInterval 0 1, event = pure id}
-          eventX = MkSignalEvent { support = MkInterval 0 1, event = pure ()}
-        split eventF eventX `shouldBe` [eventX]
+          checkLeftUnitalLaw x = (mempty <> x) `shouldEqualSignal` x
+          checkRightUnitalLaw x = (x <> mempty) `shouldEqualSignal` x
+          checkAssociativeLaw x y z = (x <> (y <> z)) `shouldEqualSignal` ((x <> y) <> z)
 
-    describe "embed" $ do
-      let pat = embed ()
-      it "should work" $ do
-        pat (MkInterval 0 1) `shouldBe`     [MkSignalEvent (MkInterval 0 1) (pure ())]
-        pat (MkInterval 0 2) `shouldBe`     [MkSignalEvent (MkInterval 0 1) (pure ()), MkSignalEvent (MkInterval 1 2) (pure ())]
-        pat (MkInterval 0.5 1.5) `shouldBe` [MkSignalEvent (MkInterval 0 1) (pure ()), MkSignalEvent (MkInterval 1 2) (pure ())]
-        pat (MkInterval 0.5 2.5) `shouldBe` [MkSignalEvent (MkInterval 0 1) (pure ()), MkSignalEvent (MkInterval 1 2) (pure ()), MkSignalEvent (MkInterval 2 3) (pure ())]
+        it "obeys the left unital law" $ do
+          checkLeftUnitalLaw (embed ())
+          checkLeftUnitalLaw (fast 2 $ embed ())
 
-      it "starts of events should be less than query ends" $ property $ \query@MkInterval{end} ->
-          [] == filter (\MkSignalEvent { support = MkInterval s _ } -> s >= end) (pat query)
+        it "obeys the right unital law" $ do
+          checkRightUnitalLaw (embed ())
+          checkRightUnitalLaw (fast 2 $ embed ())
 
-      it "ends of events should be greater than query starts" $ property $ \query@MkInterval{start} ->
-          [] == filter (\MkSignalEvent { support = MkInterval _ e } -> e <= start) (pat query)
-
-      describe "when pruned" $ do
-        let pat = embed () & prune
-
-        it "should have transparently divisible queries" $ do
-          (pat (MkInterval 0 0)   <> pat (MkInterval 0 1)) `shouldBe` pat (MkInterval 0 1)
-          (pat (MkInterval 0 1)   <> pat (MkInterval 1 1)) `shouldBe` pat (MkInterval 0 1)
-          (pat (MkInterval 0 0.5) <> pat (MkInterval 0.5 1.0)) `shouldBe` pat (MkInterval 0 1)
-          (pat (MkInterval 0 0.3) <> pat (MkInterval 0.3 1.3) <> pat (MkInterval 1.3 2)) `shouldBe` pat (MkInterval 0 2)
+        it "obeys the associative law" $ do
+          checkAssociativeLaw (embed "a") (embed "b") (embed "c")
+          checkAssociativeLaw (fast 2 $ embed "a") (fast 3 $ embed "b") (fast 5 $ embed "c")
 
     describe "fast" $ do
       let pat = embed ()
       it "should noop for fast 1" $ do
-        (fast 1 pat) (MkInterval 0 1)  `shouldBe` pat (MkInterval 0 1)
+        signal (fast 1 pat) (0, 1)  `shouldBe` signal pat (0, 1)
 
       it "should work for fast 2" $ do
-        (fast 2 pat) (MkInterval 0 0.5) `shouldBe` [MkSignalEvent (MkInterval 0 0.5) (pure ())]
-        (fast 2 pat) (MkInterval 0 1) `shouldBe`   [MkSignalEvent (MkInterval 0 0.5) (pure ()), MkSignalEvent (MkInterval 0.5 1) (pure ())]
-        (fast 2 pat) (MkInterval 1 2) `shouldBe`   [MkSignalEvent (MkInterval 1 1.5) (pure ()), MkSignalEvent (MkInterval 1.5 2) (pure ())]
+        signal (fast 2 pat) (0, 0.5) `shouldBe` [MkEvent (0, 0.5) ()]
+        signal (fast 2 pat) (0, 1) `shouldBe`   [MkEvent (0, 0.5) (), MkEvent (0.5, 1) ()]
+        signal (fast 2 pat) (1, 2) `shouldBe`   [MkEvent (1, 1.5) (), MkEvent (1.5, 2) ()]
 
       it "should work for fast 3" $ do
-        (fast 3 pat) (MkInterval 0 (1/3)) `shouldBe`     [MkSignalEvent (MkInterval 0 (1/3)) (pure ())]
-        (fast 3 pat) (MkInterval 0 1) `shouldBe`         [MkSignalEvent (MkInterval 0 (1/3)) (pure ()), MkSignalEvent (MkInterval (1/3) (2/3)) (pure ()), MkSignalEvent (MkInterval (2/3) 1) (pure ())]
-        (fast 3 pat) (MkInterval (2/3) (4/3)) `shouldBe` [MkSignalEvent (MkInterval (2/3) 1) (pure ()), MkSignalEvent (MkInterval 1 (4/3)) (pure ())]
+        signal (fast 3 pat) (0, (1/3)) `shouldBe` [MkEvent (0, (1/3)) ()]
+        signal (fast 3 pat) (0, 1) `shouldBe`     [MkEvent (0, (1/3)) (), MkEvent ((1/3), (2/3)) (), MkEvent ((2/3), 1) ()]
+        signal (fast 3 pat) ((2/3), (4/3)) `shouldBe` [MkEvent ((2/3), 1) (), MkEvent (1, (4/3)) ()]
 
-      it "should noop for fast 0.5" $ do
-        (fast 0.5 pat) (MkInterval 0 1) `shouldBe` [MkSignalEvent (MkInterval 0 2) (pure ())]
-        (fast 0.5 pat) (MkInterval 0 2) `shouldBe` [MkSignalEvent (MkInterval 0 2) (pure ())]
+      it "should work for fast 0.5" $ do
+        signal (fast 0.5 pat) (0, 1) `shouldBe` [MkEvent (0, 2) ()]
+        signal (fast 0.5 pat) (0, 2) `shouldBe` [MkEvent (0, 2) ()]
+
+      it "should work with monoid" $ do
+        signal ((fast 2 $ embed "a") <> (embed "b")) (0, 1) `shouldBe` [MkEvent (0, 0.5) "ab", MkEvent (0.5, 1) "ab"]
+        signal ((embed "a") <> (fast 2 $ embed "b")) (0, 1) `shouldBe` [MkEvent (0, 0.5) "ab", MkEvent (0.5, 1) "ab"]
+        signal ((fast 2 $ embed "a") <> (fast 2 $ embed "b")) (0, 1) `shouldBe` [MkEvent (0, 0.5) "ab", MkEvent (0.5, 1) "ab"]
 
     describe "shift" $ do
       let pat = embed ()
       it "should noop for shift 0" $ do
-        (shift 0 pat) (MkInterval 0 1)  `shouldBe` pat (MkInterval 0 1)
+        signal (shift 0 pat) (0, 1)  `shouldBe` signal pat (0, 1)
 
       it "should work" $ do
-        (shift 0 pat)   (MkInterval 0 1) `shouldBe`   [MkSignalEvent (MkInterval 0 1) (pure ())]
-        (shift 0.5 pat) (MkInterval 0 1) `shouldBe`   [MkSignalEvent (MkInterval (-1/2) (1/2)) (pure ()), MkSignalEvent (MkInterval (1/2) (3/2)) (pure ())]
-        (shift 1 pat)   (MkInterval 0 1) `shouldBe`   [MkSignalEvent (MkInterval 0 1) (pure ())]
+        signal (shift 0 pat)   (0, 1) `shouldBe` [MkEvent (0, 1) ()]
+        signal (shift 0.5 pat) (0, 1) `shouldBe` [MkEvent ((-1/2), (1/2)) (), MkEvent ((1/2), (3/2)) ()]
+        signal (shift 1 pat)   (0, 1) `shouldBe` [MkEvent (0, 1) ()]
 
       it "should shift forwards in time" $ do
-        (shift 0.25 pat) (MkInterval 0 1)  `shouldBe` [MkSignalEvent (MkInterval (-3/4) (1/4)) (pure ()), MkSignalEvent (MkInterval (1/4) (5/4)) (pure ())]
+        signal (shift 0.25 pat) (0, 1) `shouldBe` [MkEvent ((-3/4), (1/4)) (), MkEvent ((1/4), (5/4)) ()]
 
     describe "stack" $ do
       let pat = embed ()
       it "should stack patterns" $ do
-        stack [(shift 0.25 pat), (shift 0.5 pat)] (MkInterval 0 1) `shouldBe`
-          [ MkSignalEvent (MkInterval (-3/4) (1/4)) (pure ())
-          , MkSignalEvent (MkInterval (1/4) (5/4)) (pure ())
-          , MkSignalEvent (MkInterval (-1/2) (1/2)) (pure ())
-          , MkSignalEvent (MkInterval (1/2) (3/2)) (pure ())
+        signal (stack [(shift 0.25 pat), (shift 0.5 pat)]) (0, 1) `shouldBe`
+          [ MkEvent ((-3/4), (1/4)) ()
+          , MkEvent ((1/4), (5/4)) ()
+          , MkEvent ((-1/2), (1/2)) ()
+          , MkEvent ((1/2), (3/2)) ()
           ]
 
     describe "interleave" $ do
       let pat = embed ()
       it "should noop for 1" $ do
-        interleave [pat] (MkInterval 0 1) `shouldBe` pat (MkInterval 0 1)
+        signal (interleave [pat]) (0, 1) `shouldBe` signal pat (0, 1)
 
       it "should stack patterns, shifted" $ do
-        interleave [pat, pat]      (MkInterval 0 1) `shouldBe` stack [(shift 0 pat), (shift 0.5 pat)] (MkInterval 0 1)
-        interleave [pat, pat, pat] (MkInterval 0 1) `shouldBe` stack [(shift 0 pat), (shift (1/3) pat), (shift (2/3) pat)] (MkInterval 0 1)
-
-    -- describe "ap" $ do
-    --   let pat = embed ()
-
-    --   it "should noop for pure id" $ do
-    --     (ap (embed id) pat) (0, 1)  `shouldBe` [MkSignalEvent (0, 1) ()]
+        signal (interleave [pat, pat])      (0, 1) `shouldBe` signal (stack [(shift 0 pat), (shift 0.5 pat)]) (0, 1)
+        signal (interleave [pat, pat, pat]) (0, 1) `shouldBe` signal (stack [(shift 0 pat), (shift (1/3) pat), (shift (2/3) pat)]) (0, 1)
