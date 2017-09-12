2df5676c5533c978cf9fbe596ed270d3c6b999e5
Sean Lee <freshdried@gmail.com>
2017-09-12 01:53:32 -0400

    test core

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -26,6 +26,17 @@ shouldBeAround value (expectedValue, tolerance) =
       <> ", when a difference of "
       <> show difference <> " was found"
 
+shouldBeLessThan :: (HasCallStack, Show a, Ord a) => a -> a -> IO ()
+shouldBeLessThan x y =
+  if x < y
+  then return ()
+  else Test.Hspec.Expectations.expectationFailure
+    $ "expected "
+    <> show x
+    <> " to be less than "
+    <> show y
+
+
 -- | Serves a mock OSC server over UDP
 withMockOSCServer :: (OSCBundle -> IO ()) -> (Network.PortNumber -> IO a) -> IO a
 withMockOSCServer handleOSCBundle cont = do
