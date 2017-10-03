9c4af9500696f2fa2380b8b1ca2e2ed101d4eeee
sean lee <freshdried@gmail.com>
2017-10-02 23:11:51 -0400

    SuperDirt -> OSC, OSC Absolute timestamps (#24)
    * rename
    
    * fix tests
    
    * one payload per bundle
    
    * revert
    
    * reformat
    
    * wip
    
    * osc
    
    * absolute value tests

---
+++ src/Syzygy/OSC.hs
@@ -0,0 +1,75 @@
+module Syzygy.OSC where
+
+import Control.Concurrent.MVar
+import qualified Data.ByteString as BS
+import qualified Network.Socket as Network
+import qualified Network.Socket.ByteString as NetworkBS
+import qualified Vivid.OSC as OSC
+
+import Syzygy.Signal
+import Syzygy.Core
+
+toOSCBundle :: (Integer, [OSC.OSC]) -> BS.ByteString
+toOSCBundle (timeNs, message) = OSC.encodeOSCBundle $ OSC.OSCBundle timestamp (Right <$> message)
+  where
+    timestamp = OSC.Timestamp (fromInteger timeNs / 10^9)
+
+data OSCConfig = MkOSCConfig
+  { portNumber :: Network.PortNumber
+  , bpmRef :: MVar Int
+  , signalRef :: MVar (Signal [OSC.OSC])
+  , beatRef :: MVar Rational
+  }
+
+-- | nanoseconds between 1970-01-01 and 1900-01-01
+epochOffset :: Integer
+epochOffset = 2208988800 * 10^9
+
+toAbsoluteTime :: Int -> Rational -> Integer -> Event a -> (Integer, a)
+toAbsoluteTime bpm beatStart clock MkEvent{interval=(eventStart, _), payload} =
+  let
+    offset :: Integer
+    offset = floor $ (10^9 * 60) * (eventStart - beatStart) / fromIntegral bpm
+
+    time :: Integer
+    time = clock + offset + epochOffset
+  in
+    (time, payload)
+
+makeLocalUDPConnection :: Network.PortNumber -> IO Network.Socket
+makeLocalUDPConnection portNumber = do
+  (a:_) <- Network.getAddrInfo Nothing (Just "127.0.0.1") (Just (show portNumber))
+  socket <- Network.socket (Network.addrFamily a) Network.Datagram Network.defaultProtocol
+  Network.connect socket (Network.addrAddress a)
+  return socket
+
+makeOSCEnv :: OSCConfig -> IO (Env [OSC.OSC])
+makeOSCEnv MkOSCConfig{portNumber, bpmRef} = do
+  socket <- makeLocalUDPConnection portNumber
+  let
+    sendEvents :: Rational -> Integer -> [ Event [OSC.OSC] ] -> IO ()
+    sendEvents beat clock events = do
+      bpm <- readMVar bpmRef
+      let oscEvents = [toAbsoluteTime bpm beat clock event | event <- events]
+      _ <- traverse (NetworkBS.send socket . toOSCBundle) oscEvents
+      return ()
+  return MkEnv { sendEvents }
+
+backend :: Backend OSCConfig [OSC.OSC]
+backend = MkBackend {toCoreConfig, makeEnv}
+  where
+    toCoreConfig :: OSCConfig -> CoreConfig [OSC.OSC]
+    toCoreConfig MkOSCConfig {bpmRef, signalRef, beatRef} =
+      MkCoreConfig {bpmRef, signalRef, beatRef}
+
+    makeEnv :: OSCConfig -> IO (Env [OSC.OSC])
+    makeEnv = makeOSCEnv
+
+main :: IO ()
+main = do
+  bpmRef <-  newMVar 240
+  signalRef <- newMVar $ fast 16 $ embed [OSC.OSC "/play2" [OSC.OSC_S "s", OSC.OSC_S "bd"]]
+  beatRef <- newMVar 0
+  let portNumber = 57120
+  let config = MkOSCConfig { bpmRef, signalRef, beatRef, portNumber }
+  runBackend backend config
---
+++ test/Syzygy/OSCSpec.hs
@@ -0,0 +1,103 @@
+module Syzygy.OSCSpec where
+
+import Control.Concurrent
+import TestUtils (shouldBeLessThan, mean)
+import Data.Function ((&))
+import Test.Hspec
+import Vivid.OSC (OSCBundle(..), decodeOSCBundle, OSCDatum(OSC_S), OSC(..), Timestamp(..), utcToTimestamp)
+import qualified Data.Time as Time
+import Control.Monad
+import qualified Network.Socket as Network
+import qualified Network.Socket.ByteString as NetworkBS
+import qualified Data.ByteString as BS
+
+import Syzygy.OSC
+import Syzygy.Signal
+import Syzygy.Core
+
+diffTimestamp :: Timestamp -> Timestamp -> Double
+diffTimestamp (Timestamp x) (Timestamp y) = x - y
+
+data TestContext = MkTestContext
+  { receiveOSCBundle :: forall a. (OSCBundle -> IO a) -> IO a
+  , getMessage :: IO OSC
+  , getTimestamp :: IO Timestamp
+  }
+
+withMockOSCServer :: (OSCBundle -> IO ()) -> (Network.PortNumber -> IO a)  -> IO a
+withMockOSCServer handleOSCBundle continuation = do
+  address <- head <$> Network.getAddrInfo Nothing (Just "127.0.0.1") (Just (show Network.aNY_PORT))
+  socket <- Network.socket (Network.addrFamily address) Network.Datagram Network.defaultProtocol
+  Network.bind socket (Network.addrAddress address)
+  threadId <- forkIO $ forever $ do
+    msg <- NetworkBS.recv socket 4096
+    let Right bundle = decodeOSCBundle msg
+    handleOSCBundle bundle
+  portNumber <- Network.socketPort socket
+  result <- continuation portNumber
+  killThread threadId
+  return result
+
+withMockOSC :: Signal [OSC] -> Int -> (TestContext -> IO a) -> IO a
+withMockOSC defaultSignal bpm continuation = do
+  (bundleChan :: MVar OSCBundle) <- newEmptyMVar
+  withMockOSCServer (putMVar bundleChan) $ \portNumber -> do
+    bpmRef <- newMVar bpm
+    signalRef <- newMVar defaultSignal
+    beatRef <- newMVar 0
+    let config = MkOSCConfig{portNumber, bpmRef, signalRef, beatRef}
+    clientThread <- forkIO $ runBackend backend config
+    let receiveOSCBundle bundleHandler = do
+          bundleVal <- takeMVar bundleChan
+          bundleHandler bundleVal
+    let getMessage = receiveOSCBundle $ \(OSCBundle _ [Right message]) -> return message
+    let getTimestamp = receiveOSCBundle $ \(OSCBundle timestamp _) -> return timestamp
+    result <- continuation MkTestContext{receiveOSCBundle, getMessage, getTimestamp}
+    killThread clientThread
+    return result
+
+makeSuperDirtMessage :: BS.ByteString -> OSC
+makeSuperDirtMessage sound = OSC "/play2" message
+  where
+    message :: [OSCDatum]
+    message = [OSC_S "s", OSC_S sound]
+
+spec :: Spec
+spec = do
+  describe "OSC backend" $ do
+    let bpm = 240
+    let signal = nest [embed [makeSuperDirtMessage "bd"], embed [makeSuperDirtMessage "sn"]]
+
+    it "sends events with the right data" $ do
+      withMockOSC signal bpm $ \MkTestContext{getMessage} -> do
+        message <- getMessage
+        message `shouldBe` (OSC "/play2" [OSC_S "s", OSC_S "bd"])
+
+        message <- getMessage
+        message `shouldBe` (OSC "/play2" [OSC_S "s", OSC_S "sn"])
+
+    it "sends events at the right tempo, with an average jitter of less than 500ns" $ do
+      withMockOSC signal bpm $ \MkTestContext{getTimestamp} -> do
+        deltas <- (sequence $ replicate 12 $ getTimestamp)
+          & fmap (\timestamps -> zipWith diffTimestamp (tail timestamps) timestamps)
+        let
+          expectedTimeDifference :: Double
+          expectedTimeDifference = 60/fromIntegral bpm/2
+        let
+          error :: [Double]
+          error = zipWith (\x y -> abs(x - y)) (repeat expectedTimeDifference) deltas
+        mean error `shouldBeLessThan` 0.5e-6
+
+    it "sends events with timestamps with less than 2ms of latency" $ do
+      withMockOSC signal bpm $ \MkTestContext{getTimestamp} -> do
+        timestamp <- getTimestamp
+        now <- utcToTimestamp <$> Time.getCurrentTime
+        (abs $ now `diffTimestamp` timestamp) `shouldBeLessThan` 2e-3
+
+        timestamp <- getTimestamp
+        now <- utcToTimestamp <$> Time.getCurrentTime
+        (abs $ now `diffTimestamp` timestamp) `shouldBeLessThan` 2e-3
+
+        timestamp <- getTimestamp
+        now <- utcToTimestamp <$> Time.getCurrentTime
+        (abs $ now `diffTimestamp` timestamp) `shouldBeLessThan` 2e-3
