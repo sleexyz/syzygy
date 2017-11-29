e9a221c2391ab01dfe73f03c160af0ad107842e6
Sean Lee <freshdried@gmail.com>
2017-11-29 02:16:40 -0500

    sunnyvale

--- Jam.hs
+++ Jam.hs
@@ -28,80 +28,73 @@ sigMod :: Signal ParamMap -> Signal ParamMap
 sigMod = let (>>) = (flip (.)) in do
   id
   id
-  overlay $ do
-    ctrl 1 127
-    s [vmap (+(x)) | x <- palindrome [120..127]]
-
-  overlay $ do
-    ctrl 60 (80)
-    s [vmap (+1), id] & tt (1/8)
-    s [vmap (+x) | x <- [1..4]]
-    vmap (+(-9))
-    m
-
-  -- s [i, shift 0.5, fast 2, i]
-
-  overlay $ do
-    ctrl 70 (20)
-    s [vmap (+x) | x <- [0..3]] & tt (1/4)
-    -- s [slow 2, i] & tt (1/16)
-    s [vmap (+12)] & tt (1/1)
-    shift 0.5
-    m
-    -- s [fast 2, fast 1] & tt (1/16)
-    -- s [i, shift 0.5, shift 0.5, i] & tt (1)
-    -- s [i, i, fast 2, i] & tt (1/2)
-
 
 
   overlay $ do
     overlay $ do
+    -- A
       note 7 (60)
-      fast 2
+      fast 1
       overlay $ do
         ctrl 7 0
         s [vmap (+x) | x <- [0, 3]] & tt (1/4)
         -- s [vmap (+x) | x <- [0, 1]] & tt (1/2)
       vmap (+100)
+      -- pmap (+12)
       overlay $ do
         note 0 (38)
         fast 2
       -- vmap (+10)
-      -- s [shift 0.5, i]
-      -- slow 1.5
-      -- s [i, pmap (+7) . rep 1] & tt 2
-      -- s [i, pmap (subtract 2)] & tt (1/4)
-      -- s [i, pmap (subtract 5)] & tt (1/8)
-
-    -- nhk
-    overlay $ do
-      note 6 (60)
-      slow 4
-      overlay $ do
-        ctrl 6 60
-        s [vmap (+x) | x <- [6, 15]] & tt (1/16)
-      rep 1
-      rep 2
-      t [ shift 0.5 >>> rep 4, i] 2
+      s [i, slow 1.5] & tt 2
       s [m, m]
 
-    -- s [i, pmap (subtract 2)] & tt (1/8)
-    -- s [i, pmap (subtract 5)] & tt (1/4)
-
+    -- B
     overlay $ do
       note 5 (60)
+      fast 4
       overlay $ do
-        s [ctrl 5 x | x <- [22]]
-      s [pmap (+12), i]
+        s [ctrl 5 x | x <- [19]]
+      s [pmap (+12), i] & tt 1
       s [vmap (+2), i] & tt (1/4)
       s [vmap (+2), i] & tt (1/2)
       s [i, vmap (+2)] & tt (1)
+      t [s [i, m] & tt 2, i] 4
+      vmap (+(-1))
+      t [m, m] 1
+
+    -- C
+    overlay $ do
+      note 6 (60)
+      slow 1
+      overlay $ do
+        ctrl 6 60
+        s [vmap (+x) | x <- [15, 45]] & tt (1/8)
+      rep 1
+      rep 2
+      -- t [shift 0.5 >>> rep 4, i] 4
+      -- shift 0.5
+      s [m, m]
+
+    -- D
+    overlay $ do
+      note 8 (60)
+      s [fast 1, fast 4, fast 1, fast 4] 
+      slow 4
+      overlay $ do
+        ctrl 8 0
+        s [vmap (+x) | x <- [15, 16]] & tt (1/2)
+        s [vmap (+20), vmap (+30)] & tt (1/8)
+      rep 1
+      rep 2
+      t [shift 0.5 >>> rep 4, i] 4
+      s [pmap (+12), pmap (+12), pmap (+12)] & tt 1
       s [i, i]
 
-    t [shift 0.5, i] 8
+
+    -- t [shift 0.5, i] 8
     -- t [fast 2, i, fast 2, i] 2
 
-    s [pmap (+x) | x <- [0,-2]] & tt (1/16)
+    -- s [pmap (+x) | x <- [0,-2]] & tt (1/16)
     -- s [pmap (+x) | x <- [0, -5]] & tt (1/4)
     -- s [pmap (+x) | x <- [0, 12]] & tt (2)
     -- pmap (+7)
@@ -112,22 +105,55 @@ sigMod = let (>>) = (flip (.)) in do
 
   overlay $ do
     ctrl 3 127
-    -- s [vmap (const x) | x <- palindrome [(127-16)..127]]
-    -- s [vmap (+(x*40)) | x <- [0,-1]]
+    s [vmap (const x) | x <- palindrome [(127-16)..127]]
+    overlay $ do
+      ctrl 1 127
+      s [vmap (+(x)) | x <- palindrome [20..127]]
+    -- s [vmap (+(x*10)) | x <- [0,-10]]  
+    -- s [fast 8, fast 4]
+    -- s [fast 2, i] & tt (2/3)
     -- fast 2
-    -- s [fast 4, i]
+    -- s [fast 2, i]
+    -- fast 2
 
   -- t [shift 0.5, i] 8
   -- s [pmap (+12), shift 0.5, i, shift 0.5] & tt (1)
 
   overlay $ do
-    note 0 (40)
-    -- s [i, overlay $ t [shift 0.25, i] 8]
-
+    note 0 (38)
+    shift 0.5
+    -- t [fast 4, shift 0.5, fast 2, fast 1] 1
     overlay $ do
-      note 0 (47)
+      note 0 (42)
       shift 0.5
-      slow 2
+      -- fast 2
+    -- t [shift 0.5, i] 2
+    -- t [fast 4, shift 0.5, fast 2, fast 1] 1
+    -- t [fast 2, overlay $ t [shift 0.25, i] 8] 2
+    -- s [pmap (+2)] & tt (1/32) -- bon
+    -- s [pmap (+5), i] & tt (4) -- wn
+    overlay $ do
+      note 0 (40)
+
+  t [shift 0.5, i] 2
+
+    -- slow 2
+
+  -- t [slow 2, i] 4
+
+  -- t [shift 0.5, i] 2
+
+  -- t [i, t [rep 2, s[pmap (+12), rep 4]] 2 ] 4
+
+    -- overlay $ do
+    --   note 0 (47)
+    --   shift 0.5
+    --   fast 4
+
+
+  -- t [shift 0.5, i] 4
+  -- t [shift 0.5, i] 2
+  -- t [shift 0.5, i] 1
 
     -- t [shift 0.5, i] 4
   -- s [i, shift 1, i, shift 0.5] & tt (2)
